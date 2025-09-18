bc9a4dd966b5960e4720b25ab0bae5cac291cc3f
paolamontserrat <111817283+paolamontserrat@users.noreply.github.com>
2025-09-17 20:15:11 -0600

    9. Elevación de estado

--- app/src/main/java/com/example/basicstatecodelab/WaterCounter.kt
+++ app/src/main/java/com/example/basicstatecodelab/WaterCounter.kt
@@ -9,6 +9,7 @@ import androidx.compose.material3.Button
 import androidx.compose.foundation.layout.Column
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.runtime.saveable.rememberSaveable
 
@@ -16,14 +17,19 @@ import androidx.compose.runtime.saveable.rememberSaveable
 
 
 @Composable
-fun WaterCounter(modifier: Modifier = Modifier) {
+fun StatelessCounter(count: Int, onIncrement: () -> Unit, modifier: Modifier = Modifier) {
     Column(modifier = modifier.padding(16.dp)) {
-        var count by rememberSaveable { mutableStateOf(0) }
         if (count > 0) {
             Text("You've had $count glasses.")
         }
-        Button(onClick = { count++ }, Modifier.padding(top = 8.dp), enabled = count < 10) {
+        Button(onClick = onIncrement, Modifier.padding(top = 8.dp), enabled = count < 10) {
             Text("Add one")
         }
     }
+}
+@Composable
+fun StatefulCounter() {
+    var count by remember { mutableStateOf(0) }
+    StatelessCounter(count, { count++ })
+    AnotherStatelessMethod(count, { count *= 2 })
 }
